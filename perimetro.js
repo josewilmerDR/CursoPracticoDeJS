8d55b4974b39e49112e66f40dd25be4dd110c3d1
josewdr <josewdr@gmail.com>
2021-07-10 16:07:51 -0600

    Calculo de figuras

---
+++ perimetro.js
@@ -0,0 +1,21 @@
+var numero = document.getElementById("botoncito")
+var calular = numero.addEventListener("click", calcularNumero);
+
+var num = document.getElementById("numero");
+function calcularNumero()
+{
+    var lado = parseInt(num.value);
+
+    var perimetro = lado * 4;
+
+    var area = lado * lado;
+
+    var peri = document.getElementById("perimetro");
+
+    peri.innerHTML = "El perímetro es " + perimetro;
+
+    var arita = document.getElementById("area");
+
+    arita.innerHTML = "El área es " + area;
+    
+}
